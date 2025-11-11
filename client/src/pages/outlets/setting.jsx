d0e83dfcc5883e5c592db3b8c3454ca934cc6906
SnakeBoss7 <rahuldharwal12005@gmail.com>
2025-11-11 22:16:04 +0530

    CHECKING GIT UPDAED OR NOT

--- client/src/pages/outlets/setting.jsx
+++ client/src/pages/outlets/setting.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './setting.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faFaceAngry } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { usePlace } from './city';
 
 const Setting = ({handleOpac}) => {
